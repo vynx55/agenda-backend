8be419e7a1ea6c8ec6f63074f9103760d58b9345
vynx55 <alesito.html@gmail.com>
2025-06-17 23:52:03 -0500

    pequeño cambio

--- src/main/java/com/proyecto/service/Impl/UsuarioService.java
+++ src/main/java/com/proyecto/service/Impl/UsuarioService.java
@@ -5,7 +5,6 @@ import com.proyecto.repository.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
